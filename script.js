a66c380b3e3beccfb6ab84506e0dac9883865fad
Irina-Gorges <r3nategr4nate@gmail.com>
2025-04-10 14:21:07 +0200

    added folders

--- script.js
+++ script.js
@@ -55,19 +55,6 @@ function renderDeleteNotes() {
     }
 }
 
-// Notizen löschen
-function getNoteTemplate(indexNote) {
-    return `<p>&#9654; ${notesTitles[indexNote]} / ${notes[indexNote]}<button onclick="removetoTrashNote(${indexNote})">erledigt</button></p>`;
-}
-
-function getTrashNoteTemplate(indexTrashNotes, indexNote) {
-    return `<div class="deleted"><p class="deletetext">&#9654; ${trashNotesTitles[indexTrashNotes]} / ${trashNotes[indexTrashNotes]}</p><button onclick="deleteNote(${indexTrashNotes}, ${indexNote})">löschen</button></div>`;
-}
-
-// function getDeleteNoteTemplate(indexDeletedNotes) {
-//     return `<p> ${deletedNotes[indexDeletedNotes]}<button onclick="finaldeleteNote(${indexDeletedNotes})">entgültig löschen</button></p>`;
-// }
-
 // Notizen hinzufügen
 function addNote() {
     // Eingabe auslesen
